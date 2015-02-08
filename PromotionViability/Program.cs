05efe7186f1febb0487c68b6bcef6d912bb2c444
Yong Wen Chua <lawliet89@gmail.com>
2015-02-08 17:27:42 +0000

    All tier 6

--- PromotionViability/Program.cs
+++ PromotionViability/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Gw2spidyApi.Requests;
 
 namespace PromotionViability
@@ -11,6 +12,17 @@ namespace PromotionViability
         {
             get
             {
+                // Dust
+                yield return new Promotion
+                {
+                    Promoted = new ItemRequest(ItemIds.CrystallineDust),
+                    QuantityYield = 23,
+                    Ingredients = new Dictionary<ItemRequest, int>
+                    {
+                        {new ItemRequest(ItemIds.IncandescentDust), 250},
+                        {new ItemRequest(ItemIds.CrystallineDust), 1}
+                    }
+                };
                 // Ancient bone
                 yield return new Promotion
                 {
@@ -23,17 +35,96 @@ namespace PromotionViability
                         {new ItemRequest(ItemIds.CrystallineDust), 5}
                     }
                 };
+                // Claws
+                yield return new Promotion
+                {
+                    Promoted = new ItemRequest(ItemIds.ViciousClaw),
+                    QuantityYield = Promotion.Tier5PromotionYield,
+                    Ingredients = new Dictionary<ItemRequest, int>
+                    {
+                        {new ItemRequest(ItemIds.ViciousClaw), 1},
+                        {new ItemRequest(ItemIds.LargeClaws), 50},
+                        {new ItemRequest(ItemIds.CrystallineDust), 5}
+                    }
+                };
+                // Fangs
+                yield return new Promotion
+                {
+                    Promoted = new ItemRequest(ItemIds.ViciousFang),
+                    QuantityYield = Promotion.Tier5PromotionYield,
+                    Ingredients = new Dictionary<ItemRequest, int>
+                    {
+                        {new ItemRequest(ItemIds.ViciousFang), 1},
+                        {new ItemRequest(ItemIds.LargeFang), 50},
+                        {new ItemRequest(ItemIds.CrystallineDust), 5}
+                    }
+                };
+                // Scales
+                yield return new Promotion
+                {
+                    Promoted = new ItemRequest(ItemIds.ArmoredScale),
+                    QuantityYield = Promotion.Tier5PromotionYield,
+                    Ingredients = new Dictionary<ItemRequest, int>
+                    {
+                        {new ItemRequest(ItemIds.ArmoredScale), 1},
+                        {new ItemRequest(ItemIds.LargeScale), 50},
+                        {new ItemRequest(ItemIds.CrystallineDust), 5}
+                    }
+                };
+                // Totems
+                yield return new Promotion
+                {
+                    Promoted = new ItemRequest(ItemIds.ElaborateTotem),
+                    QuantityYield = Promotion.Tier5PromotionYield,
+                    Ingredients = new Dictionary<ItemRequest, int>
+                    {
+                        {new ItemRequest(ItemIds.ElaborateTotem), 1},
+                        {new ItemRequest(ItemIds.IntricateTotem), 50},
+                        {new ItemRequest(ItemIds.CrystallineDust), 5}
+                    }
+                };
+                // Venom sacs
+                yield return new Promotion
+                {
+                    Promoted = new ItemRequest(ItemIds.PowerfulVenomSac),
+                    QuantityYield = Promotion.Tier5PromotionYield,
+                    Ingredients = new Dictionary<ItemRequest, int>
+                    {
+                        {new ItemRequest(ItemIds.PowerfulVenomSac), 1},
+                        {new ItemRequest(ItemIds.PotentVenomSac), 50},
+                        {new ItemRequest(ItemIds.CrystallineDust), 5}
+                    }
+                };
+                // Blood
+                yield return new Promotion
+                {
+                    Promoted = new ItemRequest(ItemIds.PowerfulBlood),
+                    QuantityYield = Promotion.Tier5PromotionYield,
+                    Ingredients = new Dictionary<ItemRequest, int>
+                    {
+                        {new ItemRequest(ItemIds.PowerfulBlood), 1},
+                        {new ItemRequest(ItemIds.PotentBlood), 50},
+                        {new ItemRequest(ItemIds.CrystallineDust), 5}
+                    }
+                };
             }
         }
 
         private static void Main(string[] args)
         {
+            Console.WriteLine("Request all items data from GW2Spidy (limitation)");
             // Get all requests
             var allItems = new AllItemsRequest();
-            allItems.Get();
+            var loadingTask = allItems.Send();
+            while (!loadingTask.IsCompleted)
+            {
+                Console.Write(".");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine();
             foreach (var promotion in Promotions)
             {
-                Console.WriteLine("Promoting {0}:", promotion.Name);
+                Console.WriteLine("Promoting to {0}:", promotion.Name);
                 Console.WriteLine("\tCost of ingredients: {0}", promotion.CostOfIngridients);
                 Console.WriteLine("\tProfit from selling an average yield of {0}: {1}", 
                     promotion.QuantityYield, promotion.ProfitOfProduct);
